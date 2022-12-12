1a883808f4b3c900c90a8ca862bb2762b12c4121
mura012 <murakoudai@gmail.com>
2022-12-12 11:00:57 +0900

    initialcommit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hello world!
+      </h1>
+    </>
+  );
+}
